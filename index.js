514267fdc3fd425e57d0842e293d357f70b3d83a
Kumbukani Kamanga <152120171097@ogrenci.ogu.edu.tr>
2021-01-28 10:07:23 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const { render } = require("ejs");
+const express = require("express");
+
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');   
+app.set('views', path.join(__dirname, '/views'))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(3000, () => {
+  console.log('App is listening on port:3000')
+})
